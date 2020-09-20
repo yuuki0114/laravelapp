432d4778ae7335ef3a64f7f52ef09573f0c5a86c
yuuki0114 <y.no0114@gmail.com>
2020-09-20 12:55:09 +0900

    ビューとテンプレート

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,11 +9,11 @@ class HelloController extends Controller
 {
     public function index()
     {
-        $data = ['one', 'two', 'three', 'four', 'five'];
+        $data = [
+            ['name' => '山田たろう', 'mail' => 'taro@yamada'],
+            ['name' => '田中はなこ', 'mail' => 'hanako@flower'],
+            ['name' => '鈴木さちこ', 'mail' => 'sachico@happy'],
+        ];
         return view('hello.index', ['data' => $data]);
-    }
-    public function post(Request $request)
-    {
-        return view('hello.index', ['msg' => $request-> msg]);
     }
 }
